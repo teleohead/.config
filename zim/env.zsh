97d10c141672e0e5f58fcee3dad6ed9e038d2a5f
Yubing Zhang <joseph.zyb@gmail.com>
2023-10-05 09:59:19 +0800

    Major update.

--- zim/env.zsh
+++ zim/env.zsh
@@ -1,7 +1,5 @@
 export EDITOR=nvim
 export XDG_CONFIG_HOME=$HOME/.config/
 export RANGER_LOAD_DEFAULT_RC="false"
-export TOOLCHAINS=swift
-export PATH="/opt/homebrew/opt/openjdk@11/bin:$PATH"%
-source /Users/yz/k/k.sh
 export ZIM_HOME=$XDG_CONFIG_HOME/zim/
+export PATH="/opt/homebrew/bin:$PATH"
