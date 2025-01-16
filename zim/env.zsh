9b02d656cfcf127b9165e227a0e3e0267b53b5f7
Yubing Zhang <joseph.zyb@gmail.com>
2025-01-16 08:31:38 +0800

    Cleanup before adding new functionality.

--- zim/env.zsh
+++ zim/env.zsh
@@ -3,4 +3,18 @@ export XDG_CONFIG_HOME=$HOME/.config/
 export RANGER_LOAD_DEFAULT_RC="false"
 export ZIM_HOME=$XDG_CONFIG_HOME/zim/
 export PATH=$PATH:/opt/homebrew/bin
-export PATH="/opt/homebrew/opt/ruby@3.2/bin:$PATH"
+
+# RUBY on macOS
+export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/ruby/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/ruby/include"
+export PATH="/opt/homebrew/lib/ruby/gems/3.3.0/bin:$PATH"
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+[[ ! -r '/Users/teleohead/.opam/opam-init/init.zsh' ]] || source '/Users/teleohead/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
+# END opam configuration
--- zim/init.zsh
+++ zim/init.zsh
@@ -1,5 +1,5 @@
-zimfw() { source /Users/teleohead/.config/zim/zimfw.zsh "${@}" }
-zmodule() { source /Users/teleohead/.config/zim/zimfw.zsh "${@}" }
+zimfw() { source /Users/teleohead/.config//zim//zimfw.zsh "${@}" }
+zmodule() { source /Users/teleohead/.config//zim//zimfw.zsh "${@}" }
 fpath=(/Users/teleohead/.config/zim/modules/git/functions /Users/teleohead/.config/zim/modules/utility/functions /Users/teleohead/.config/zim/modules/git-info/functions /Users/teleohead/.config/zim/modules/duration-info/functions /Users/teleohead/.config/zim/modules/prompt-pwd/functions ${fpath})
 autoload -Uz -- git-alias-lookup git-branch-current git-branch-delete-interactive git-branch-remote-tracking git-dir git-ignore-add git-root git-stash-clear-interactive git-stash-recover git-submodule-move git-submodule-remove mkcd mkpw coalesce git-action git-info duration-info-precmd duration-info-preexec prompt-pwd
 source /Users/teleohead/.config/zim/modules/environment/init.zsh
